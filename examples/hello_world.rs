57de5230f054f2e8a6f5a8d92dabb0ce2d796ad8
DiscreteTom <discrete_tom@outlook.com>
2024-10-10 06:27:47 +0000

    examples: update hello_world

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -2,13 +2,8 @@ use aws_lambda_runtime_proxy::Proxy;
 
 #[tokio::main]
 async fn main() {
+  // create the proxy server and the handler process using the default configuration
   let proxy = Proxy::default().spawn().await.unwrap();
-  // equals to:
-  // let proxy = Proxy::default()
-  //   .port(3000)
-  //   .command(Proxy::default_command())
-  //   .spawn()
-  //   .await;
-
+  // forward all requests to the real Lambda Runtime API
   proxy.server.passthrough().await;
 }
